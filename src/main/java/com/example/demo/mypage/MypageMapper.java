f0ca1214c269c32730e28ef3112ead7783003cd9
sylee0915 <seungyup8@gmail.com>
2025-09-06 19:53:33 +0900

    feat: make package

---
+++ src/main/java/com/example/demo/mypage/MypageMapper.java
@@ -0,0 +1,8 @@
+package com.example.demo.mypage;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public class MypageMapper {
+
+}
---
+++ src/main/java/com/example/demo/mypage/MypageService.java
@@ -0,0 +1,12 @@
+package com.example.demo.mypage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MypageService {
+	
+	@Autowired
+	private MypageMapper mapper;
+
+}
