bc13ef5b38da2e5089578037f13743615763e3df
sylee0915 <seungyup8@gmail.com>
2025-09-18 18:40:05 +0900

    feat: checkpoint1840

--- src/main/java/com/example/backend/searchRestApi/SearchRepository.java
+++ src/main/java/com/example/backend/searchRestApi/SearchRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.backend.api.Hotels;
 
-public interface SearchRepository extends JpaRepository<Hotels, Long>, SearchRepositoryCustom {
+public interface SearchRepository extends JpaRepository<Hotels, String>, SearchRepositoryCustom {
     SearchResponseDto findBySearchElements(SearchRequestDto searchRequest);
 }
